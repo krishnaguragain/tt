aa6f919134726fc4cf72ccd09e5e4277be2a070c
Krishna Guragai <mailkrishnaguragai@gmail.com>
2015-08-07 21:49:36 +0545

    added place search

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+this.imagePreview = function(){	
+		/* CONFIG */
+		xOffset = 10;
+		yOffset = 30;
+	$("img.preview").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='preview'><img src='"+ this.getAttribute('data-img')+"' alt='Image preview' />"+ c +"</p>");								 
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#preview").remove();
+    });	
+	$("img.preview").mousemove(function(e){
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+$(document).ready(function(){
+	imagePreview();
+});
